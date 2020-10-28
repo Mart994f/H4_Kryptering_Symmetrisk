65cf1a5fa00e066f8addc67514dfe8ce6d0fa4cf
Martin Rosenquist <mrquist_94@hotmail.com>
2020-10-28 19:21:18 +0100

    Initial Commit

---
+++ SymmetricEncryption/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Text;
+
+namespace SymmetricEncryption
+{
+    class Program
+    {
+        static SymmetricEncrypter encrypter;
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo selectInput;
+            bool appIsRunning = true;
+
+            while (appIsRunning)
+            {
+                Console.Clear();
+                Console.WriteLine("Symmectric Encryption & Decryption\n");
+
+                Console.WriteLine("[1] DES");
+                Console.WriteLine("[2] TripleDES");
+                Console.WriteLine("[3] AES");
+                Console.WriteLine("[0] EXIT");
+
+                Console.Write("\nSELECT: ");
+                selectInput = Console.ReadKey();
+
+                switch (selectInput.Key)
+                {
+                    case ConsoleKey.D0:
+                    case ConsoleKey.NumPad0:
+                        appIsRunning = false;
+                        break;
+                    case ConsoleKey.D1:
+                    case ConsoleKey.NumPad1:
+                        encrypter = new SymmetricEncrypter("DES");
+                        GetInput("DES");
+                        break;
+                    case ConsoleKey.D2:
+                    case ConsoleKey.NumPad2:
+                        encrypter = new SymmetricEncrypter("TRIPLEDES");
+                        GetInput("TrippleDES");
+                        break;
+                    case ConsoleKey.D3:
+                    case ConsoleKey.NumPad3:
+                        encrypter = new SymmetricEncrypter("AES");
+                        GetInput("AES");
+                        break;
+                }
+            }
+        }
+
+        private static void GetInput(string algorithm)
+        {
+            ConsoleKeyInfo selectInput;
+            string message;
+            byte[] key;
+            byte[] iv;
+
+            Console.Clear();
+            Console.WriteLine($"{algorithm} Encryption & Decryption\n");
+            Console.WriteLine("[1] Encrypt");
+            Console.WriteLine("[2] Decrypt");
+
+            Console.Write("\nSELECT: ");
+            selectInput = Console.ReadKey();
+
+            switch (selectInput.Key)
+            {
+                case ConsoleKey.D1:
+                case ConsoleKey.NumPad1:
+                    Console.Clear();
+                    Console.Write("\nMesssage: ");
+                    message = Console.ReadLine();
+
+                    key = encrypter.GenerateRandom(32);
+                    iv = encrypter.GenerateRandom(16);
+
+                    Console.WriteLine($"\r\nEncrypted message: {encrypter.Encrypt(Encoding.UTF8.GetBytes(message), key, iv)}");
+                    Console.WriteLine($"\nKey: {Convert.ToBase64String(key)}");
+                    Console.WriteLine($"IV: {Convert.ToBase64String(iv)}");
+                    Console.WriteLine("Press [ENTER] to continue");
+                    Console.ReadLine();
+                    break;
+                case ConsoleKey.D2:
+                case ConsoleKey.NumPad2:
+                    Console.Clear();
+                    Console.Write("\nEncrypted messsage: ");
+                    message = Console.ReadLine();
+
+                    Console.Write("\nKey: ");
+                    key = Convert.FromBase64String(Console.ReadLine());
+
+                    Console.Write("\nIV: ");
+                    iv = Convert.FromBase64String(Console.ReadLine());
+
+                    Console.WriteLine($"\r\nDecrypted message: {encrypter.Decrypt(Convert.FromBase64String(message), key, iv)}");
+                    Console.WriteLine("Press [ENTER] to continue");
+                    Console.ReadLine();
+                    break;
+            }
+        }
+    }
+}
---
+++ SymmetricEncryption/SymmetricEncrypter.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace SymmetricEncryption
+{
+    class SymmetricEncrypter
+    {
+        #region Private Fields
+
+        private SymmetricAlgorithm _algorithm;
+
+        #endregion
+
+        #region Constructors
+
+        public SymmetricEncrypter(string algorithm)
+        {
+            if (algorithm == "DES")
+                _algorithm = DES.Create();
+            if (algorithm == "TRIPLEDES")
+                _algorithm = TripleDES.Create();
+            if (algorithm == "AES")
+                _algorithm = Aes.Create();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public string Encrypt(byte[] input, byte[] key, byte[] iv)
+        {
+            _algorithm.Key = key;
+            _algorithm.IV = iv;
+
+            ICryptoTransform cryptoTransform = _algorithm.CreateEncryptor(_algorithm.Key, _algorithm.IV);
+
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (CryptoStream cryptoStream = new CryptoStream(memoryStream, cryptoTransform, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter streamWriter = new StreamWriter(cryptoStream))
+                    {
+                        streamWriter.Write(input);
+                    }
+                    return Convert.ToBase64String(memoryStream.ToArray());
+                }
+            }
+        }
+
+        public string Decrypt(byte[] input, byte[] key, byte[] iv)
+        {
+            _algorithm.Key = key;
+            _algorithm.IV = iv;
+
+            ICryptoTransform cryptoTransform = _algorithm.CreateDecryptor(_algorithm.Key, _algorithm.IV);
+
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (CryptoStream cryptoStream = new CryptoStream(memoryStream, cryptoTransform, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter streamWriter = new StreamWriter(cryptoStream))
+                    {
+                        streamWriter.Write(input);
+                    }
+                    return Convert.ToBase64String(memoryStream.ToArray());
+                }
+            }
+        }
+
+        public byte[] GenerateRandom(int length)
+        {
+            using (RandomNumberGenerator generator = RandomNumberGenerator.Create())
+            {
+                byte[] randomNumber = new byte[length];
+
+                generator.GetBytes(randomNumber);
+
+                return randomNumber;
+            }
+        }
+
+        #endregion
+    }
+}
